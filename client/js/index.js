3e6c8f175a72a91002a190703e0eedf6b133cf7f
daniil.teterevkov <daniil.teterevkov@est.fib.upc.edu>
2015-10-18 04:03:01 +0200

    Integrating leap motion with css effects

--- client/js/index.js
+++ client/js/index.js
@@ -4,9 +4,21 @@ function swipeRight() {
 
     $(product).addClass('rotate-left').delay(1500).fadeOut(1);
     if (product.is(':last-child') ) {
+        console.log("I am the last pic");
         $(product + ':nth-child(1)').removeClass('rotate-left rotate-right').fadeIn(300);
     }
     else {
+        console.log("Last pic I am not - Yoda");
         product.next().fadeIn(400);
     }
 };
+
+function wait(seconds) {
+    var milliseconds = seconds * 1000;
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+            break;
+        }
+    }
+}
