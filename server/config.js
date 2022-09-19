8c19f1da775f43929a6cbbcaccf8cd898062caba
Ricard Gascons <rgascons@users.noreply.github.com>
2022-09-19 09:32:26 -0700

    Remove old aws secret

--- server/config.js
+++ server/config.js
@@ -2,5 +2,5 @@ exports.merchantId = "hk68j9x6bbxnjm94";
 exports.publicKey = "nht5vxqhjzd82262";
 exports.privateKey = "2d105f792479c7a646f30cfd648a5d1f";
 exports.accessKeyId = "AKIAJYVXDCDIOASXVBLA";
-exports.secretAccessKey = "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";
+exports.secretAccessKey = "[aws-secret]";
 exports.associateTag = "httpgithcombu-21";
